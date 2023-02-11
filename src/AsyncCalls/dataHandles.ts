d749fd26cacd4a2e69bc84da34d40db9f4dfa917
Ben Tracey <bptracey@btinternet.com>
2023-02-11 01:07:16 +0000

    api updated to cloud render

--- src/AsyncCalls/dataHandles.ts
+++ src/AsyncCalls/dataHandles.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export const addReminderData = async (reminder: any, setError: (error: string) => void) => {
   const { date, phonenumber, message } = reminder;
   try {
-    const url = "server-production-1c8a.up.railway.app/addReminder";
+    const url = "https://reminder-server-vawt.onrender.com/addReminder";
     if (date && phonenumber && message) {
       const responseAddURL = await axios.post(url, reminder);
       console.log(responseAddURL);
@@ -18,9 +18,10 @@ export const addReminderData = async (reminder: any, setError: (error: string) =
 
 export const getReminders = async (setReminders:(responseURL:[])=>void, setError: (error: string) => void) => {
   try {
-    const getUrl = "server-production-1c8a.up.railway.app/getReminder";
+    const getUrl = "https://reminder-server-vawt.onrender.com/getReminder";
     const responseURL = await axios.get(getUrl);
     setReminders(responseURL.data);
+    console.log(responseURL.data);
   } catch (error) {
      setError("There is a Problem while Getting the Reminders");
   }
