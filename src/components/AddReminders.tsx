e79ed18b07c4043dae712c519932f9dad48379a8
Sweta Shah <swetashah.8430@gmail.com>
2023-02-20 11:25:17 +0000

    Made the app Responsive

--- src/components/AddReminders.tsx
+++ src/components/AddReminders.tsx
@@ -1,43 +1,44 @@
-import PhoneInput from 'react-phone-input-2';
-import 'react-phone-input-2/lib/style.css';
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/bootstrap.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import TimePicker, { TimePickerValue } from "react-time-picker";
+import "react-time-picker/dist/TimePicker.css";
+import "react-clock/dist/Clock.css";
 import { useState } from "react";
+import "../reminders.css";
 
 export interface IAddRemindersProps {
- addReminder: (reminder: any) => void;
+  addReminder: (reminder: any) => void;
 }
 
 export function AddReminders(props: IAddRemindersProps) {
-  
   const [phoneNo, setPhoneNo] = useState<string | undefined>(undefined);
   const [startDate, setStartDate] = useState(new Date());
   const [time, setTime] = useState<string>("10:00");
   const [message, setMessage] = useState("");
-  let dateString = startDate.toISOString().split('T')[0];
+  let dateString = startDate.toISOString().split("T")[0];
   let timeString = time + ":00Z";
   let date = `${dateString}T${timeString}`;
- 
+
   const phonenumber = "+" + phoneNo;
   const handleReminderData = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     let reminder = {
       date,
       phonenumber,
-      message
-    }
-   
+      message,
+    };
+
     setPhoneNo("");
     setStartDate(new Date());
     setTime("");
     setMessage("");
     props.addReminder(reminder);
-  }
+  };
 
-  
   return (
-    <form className="row g-3" onSubmit={ handleReminderData}>
+    <form className="row g-3" onSubmit={handleReminderData}>
       <div className="container form w-50">
         <div className="card form">
           <div className="card-header color">Add Reminders</div>
@@ -45,27 +46,42 @@ export function AddReminders(props: IAddRemindersProps) {
             <label htmlFor="phoneno" className="form-label">
               Phone No:
             </label>
-            <PhoneInput country={"gb"} value={phoneNo} onChange={setPhoneNo} />
-
+             <div style={{ maxWidth: '400px' }}>
+            <PhoneInput country={"gb"} value={phoneNo} onChange={setPhoneNo}
+            inputStyle={{ 
+            border: '1px solid #ccc',
+            borderRadius: '4px',
+            paddingLeft: '50px',
+            fontSize: '16px',
+            width: '100%',
+            margin:"0"
+        }}/></div>
             <br></br>
 
-            <label htmlFor="dateandtime" className="form-label">
-              Date and Time:
-            </label>
-            <div className="date-time">
-              <DatePicker
-                selected={startDate}
-                onChange={(date: Date) => setStartDate(date)}
-                className="form-control"
-              />
-
-              <TimePicker
-                onChange={(value: TimePickerValue) => setTime(value.toString())}
-                value={time}
-                className="form-control"
-              />
+            <div className="form-group">
+              <label htmlFor="dateandtime" className="form-label">
+                Date and Time:
+              </label>
+              <div className="row">
+                <div className="col-md-6">
+                  <DatePicker
+                    selected={startDate}
+                    onChange={(date: Date) => setStartDate(date)}
+                    className="form-control"
+                    dateFormat="P"
+                  />
+                </div>
+                <div className="col-md-6">
+                  <TimePicker
+                    onChange={(value: TimePickerValue) => setTime(value.toString())}
+                    value={time}
+                    className="form-control"
+                    required
+                    disableClock
+                  />
+                </div>
+              </div>
             </div>
-            <br></br>
 
             <label htmlFor="message" className="form-label">
               Message:
